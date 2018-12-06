cf958540a249f3ed48a44a27af8191ae442bbc71
minjichu <key0876@naver.com>
2018-12-06 16:02:44 +0900

    중간

--- linkedList.c
+++ linkedList.c
@@ -4,7 +4,7 @@ typedef struct linknode{
 	int data;
 	//void *next;
 
-	struct linknd *next
+	struct linknd *next;
 
 }linknd_t; /* linkt node의 타입 */
 
@@ -28,3 +28,28 @@ typedef struct linknode{
 
 	return ndPtr; 
  }
+ 
+ void addTail(int value){
+ 	linknd_t *ndPtr, *newPtr;
+ 	if (list == NULL)
+ 	{
+ 		return; 
+	 } /* 방어 코드 */
+	 else 
+	 {
+	 	ndPtr = list;
+	 	while (ndPtr -> next != NULL){
+	 		ndPtr = ndPtr -> next;
+		 }
+		 
+		 newPtr = create_node(value);
+		 ndPtr -> next = newPtr; /* 마지막노드가 새로운 노드 가르키는거 끝에 데이터 붙임 */ 
+	  } 
+ }
+
+void genList(void){
+ 	list = create_node(0); /* list가 노드 가리키게 전역변수 list로 데이터가 없으므로 괄호안에 0 */ 
+ 	
+ 	return; 
+} 
+ 
