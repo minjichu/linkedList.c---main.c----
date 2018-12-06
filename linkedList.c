c71499adf05fe6f8004a815092b0503d0ba8d29c
minjichu <key0876@naver.com>
2018-12-06 15:44:27 +0900

    linkedlist 실체

--- linkedList.c
+++ linkedList.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h> 
 typedef struct linknode{
 	int data;
 	//void *next;
@@ -8,3 +10,21 @@ typedef struct linknode{
 
 
  static linknd_t *list; // linked list 실체 */ 
+ 
+ linknd_t* create_node(int value) {
+ 	linknd_t* ndPtr;
+ 	
+ 	// 동적 메모리 할당 & 정수값 저장 
+	ndPtr = (linknd_t*)malloc( sizeof(linknd_t)); 
+	if (ndPtr == NULL)
+		{
+			printf ("ERROR\n");
+			return NULL;
+		}
+	
+	// 정수 값 저장 
+	ndPtr ->data = value;
+	ndPtr -> next = NULL; 	 
+
+	return ndPtr; 
+ }
