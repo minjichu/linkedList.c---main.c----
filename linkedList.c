faa0fb5896c4b011f30213b1563df8fcc9a31e06
minjichu <key0876@naver.com>
2018-12-06 16:34:57 +0900

    ㅁ마지막

--- linkedList.c
+++ linkedList.c
@@ -53,15 +53,19 @@ void genList(void){
  	return; 
 } 
  
-void print_list(void){
+void print_list(int n){
 	linknd_t *ndPtr; 
-	if (list -> next == NULL) /* 방어 코드 어차피 null이면 빠져나가서 어차피 와일문 조건에 있음 */ 
-	{
-		return;
-	}
+	int i=0;
+	
 	ndPtr =(linknd_t*) list -> next; /* warning 없애기 위해 명시적 형변환 next를 보이드로 해서 */ 
 	while(ndPtr != NULL) {
-		printf ("%i ", ndPtr -> data);
+		i++;
+		if (i==n)
+		{
+		
+		printf ("%i-th value : %i\n ",i, ndPtr -> data);
+		break;
+		}
 		ndPtr = ndPtr -> next;
 	}
 }
