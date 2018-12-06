9e927e6ddc27bd600143a8b78a9e1eac129496ba
minjichu <key0876@naver.com>
2018-12-06 15:28:03 +0900

    static

--- linkedList.c
+++ linkedList.c
@@ -1,9 +1,10 @@
 typedef struct linknode{
 	int data;
 	//void *next;
+
 	struct linknd *next
+
 }linknd_t; /* linkt node의 타입 */
 
 
-// struct linknd nd;
-linknd_t nd; 
+ static linknd_t *list; // linked list 실체 */ 
